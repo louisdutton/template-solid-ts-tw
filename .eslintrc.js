e0a240d0167ea6eaa04abf8e4e493924eb0dbf05
louisdutton <contact@louisdutton.com>
2022-06-25 13:11:16 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <main class="flex h-screen w-screen flex-col items-center justify-center">
+      <p class="text py-20 text-center text-4xl">SolidJS + TS + TailwindCSS</p>
+    </main>
+  );
+};
+
+export default App;
